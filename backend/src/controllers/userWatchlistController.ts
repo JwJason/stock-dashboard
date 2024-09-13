d1ad02cdf32b5de00fcd1ff3fb9811235e6c7241
jasonb <jw_brandt@outlook.com>
2024-09-13 12:30:46 -0400

    Improve request validation on backend

--- backend/src/controllers/userWatchlistController.ts
+++ backend/src/controllers/userWatchlistController.ts
@@ -1,38 +1,43 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { watchlistService } from "../services/watchlistService";
 import { handleError } from "../handlers/errorHandler";
 import WatchlistItemCreationRequestData from "../models/WatchlistItemCreationRequestData";
+import WatchlistItemRetrievalRequestData from "../models/WatchlistItemRetrievalRequestData";
 
 export const getWatchlistItems = (req: Request, res: Response) => {
-    try {
-        const userId = parseInt(req.params.id);
-        watchlistService.getWatchlist(userId).then((watchlist) => {
-            return res.status(200).json({
-                'watchlistItems': watchlist,
-            });
-        }).catch((err) => {
-            return handleError(err, res);
+  try {
+    const requestData =
+      WatchlistItemRetrievalRequestData.createFromRequest(req);
+    watchlistService
+      .getWatchlist(requestData.getUserId())
+      .then((watchlist) => {
+        return res.status(200).json({
+          watchlistItems: watchlist,
         });
-    } catch (err) {
+      })
+      .catch((err) => {
         return handleError(err, res);
-    }
+      });
+  } catch (err) {
+    return handleError(err, res);
+  }
 };
 
 export const createWatchlistItem = (req: Request, res: Response) => {
-    // TODO - Get userId from auth; it's currently defined in the request params
-    try {
-        const requestData = WatchlistItemCreationRequestData.createFromRequest(req);
-        watchlistService.addToWatchlist(
-            requestData.getUserId(),
-            requestData.getStockSymbol()
-        ).then((watchlist) => {
-            return res.status(201).json({
-                'watchlistItems': watchlist,
-            });
-        }).catch((err) => {
-            return handleError(err, res);
-        })
-    } catch (err) {
+  // TODO - Get userId from auth; it's currently defined in the request params
+  try {
+    const requestData = WatchlistItemCreationRequestData.createFromRequest(req);
+    watchlistService
+      .addToWatchlist(requestData.getUserId(), requestData.getStockSymbol())
+      .then((watchlist) => {
+        return res.status(201).json({
+          watchlistItems: watchlist,
+        });
+      })
+      .catch((err) => {
         return handleError(err, res);
-    }
+      });
+  } catch (err) {
+    return handleError(err, res);
+  }
 };
